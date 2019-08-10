b5b47224d222f5131cef1beccb6739900b2caf65
vscholl <victoria.scholl@colorado.edu>
2019-08-10 15:57:47 -0600

    downloads each data product linearly for now. code for looping and moving files is below. not sure if these steps are critical right now to spend time on them...

--- prep_aop_imagery.R
+++ prep_aop_imagery.R
@@ -2,33 +2,164 @@
 # and prepares them to become descriptive features for species classification.
 
 
+
+# Download AOP data -------------------------------------------------------
+
 # Download NEON AOP remote sensing tiles that cover the same area as the 
 # in-situ woody vegetation data. We can specify specific coordinates using
 # the tree locations from the woody vegetation structure data. 
 
-# List of Data Product IDs to download
-          
-data_products <- data.frame(id = c("DP3.30015.001", "DP3.30025.001", "DP3.30010.001"),
-                            name = c("chm", "aspect_slope", "rgb"))
 
-for (dpID in data_products$id){
+# API call parameters 
+site_code <- "NIWO"        # four-digit NEON site code 
+data_year <- "2017"        # four-digit year YYYY 
+dir_out <- "data/data_raw" # top-level directory to save downloaded files
+buffer_val <- 5 #[m]       # integer buffer size around tree coordinates
+
+
+
+
+# CHM ---------------------------------------------------------------------
+
+dp_chm <- "DP3.30015.001"
+
+neonUtilities::byTileAOP(
+  dpID = dp_chm
+  ,site = site_code
+  ,year = data_year
+  ,savepath = dir_out
+  ,easting = veg_coordinates$eastings
+  ,northing = veg_coordinates$northings
+  ,buffer = buffer_val)
+
+
+# aspect_slope ------------------------------------------------------------
+
+dp_aspect_slope <- "DP3.30025.001"
+
+neonUtilities::byTileAOP(
+  dpID = dp_aspect_slope
+  ,site = site_code
+  ,year = data_year
+  ,savepath = dir_out
+  ,easting = veg_coordinates$eastings
+  ,northing = veg_coordinates$northings
+  ,buffer = buffer_val)
+
+
+
+# RGB ---------------------------------------------------------------------
+
+dp_rgb <- "DP3.30015.001"
+
+neonUtilities::byTileAOP(
+  dpID = dp_rgb
+  ,site = site_code
+  ,year = data_year
+  ,savepath = dir_out
+  ,easting = veg_coordinates$eastings
+  ,northing = veg_coordinates$northings
+  ,buffer = buffer_val)
+
+
+
+# Vegetation indices ------------------------------------------------------
+
+dp_veg_indices <- "DP3.30026.001"
+
+neonUtilities::byTileAOP(
+  dpID = dp_veg_indices
+  ,site = site_code
+  ,year = data_year
+  ,savepath = dir_out
+  ,easting = veg_coordinates$eastings
+  ,northing = veg_coordinates$northings
+  ,buffer = buffer_val)
+
+
+
+# Hyperspectral reflectance -----------------------------------------------
+
+dp_hs_refl <- "DP3.30006.001"
+
+neonUtilities::byTileAOP(
+  dpID = dp_hs_refl
+  ,site = site_code
+  ,year = data_year
+  ,savepath = dir_out
+  ,easting = veg_coordinates$eastings
+  ,northing = veg_coordinates$northings
+  ,buffer = buffer_val)
+
+
+
+
+              
+
+
+
+
+# Looping through data product downloads ----------------------------------
+
+
+
+# List of Data Product IDs to download
+# "DP3.30015.001" - "chm"               LiDAR-derived Canopy Height Model
+# "DP3.30025.001" - "aspect_slope"      LiDAR-derived Aspect and Slope
+# "DP3.30010.001" - "rgb"               High-resolution multispectral camera RGB 
+# "DP3.30026.001" - "veg_indices"       Spectrometer-derived vegetation indices 
+# "DP3.30006.001" - "hs_refl"           Spectometer-derived surface reflectance
+
+data_products <- data.frame(id = c("DP3.30015.001"
+                                   ,"DP3.30025.001"
+                                   ,"DP3.30010.001"
+                                   ,"DP3.30026.001"
+                                   ,"DP3.30006.001")
+                            ,name = c("chm"
+                                      ,"aspect_slope"
+                                      ,"rgb"
+                                      ,"veg_indices"
+                                      ,"hs_refl"))
+
+
+# Can this be turned into a loop?
+# How to make R wait for user input (y/n) and wait for the files to download? 
+
+for (i in nrow(data_products)){
   
-  print(paste("Downloading data product ID", dpID))
-        
+  dp_id <- as.character(data_products$id[i])
+  dp_name <- as.character(data_products$name[i])
+  
+  print(paste("Downloading data product ID", dp_id))
+  
   neonUtilities::byTileAOP(
-    dpID = dpID
-    ,site = "NIWO"
-    ,year = "2017"
-    ,savepath = "data/data_raw"
+    dp_id = dp_id
+    ,site = site_code
+    ,year = data_year
+    ,savepath = dir_out
     ,easting = veg_coordinates$eastings
     ,northing = veg_coordinates$northings
-    ,buffer = 5)
+    ,buffer = buffer_val)
   
 } 
 
+
+
+
+# Move downloaded AOP data ------------------------------------------------
+
+move the files intuituve folder name.
+# For example, the default nested directories where RGB data is stored: 
+#   "data/data_raw/DP3.30010.001/2017/FullSite/D13/2017_NIWO_1/L3/Camera/Mosaic/V01/2017_NIWO_1_453000_4433000_image.tif"
+# to a simpler directory structure:
+#   "data/data_raw/rgb/2017_NIWO_1_453000_4433000_image.tif"
+# mainly for convenience if the user wants to quickly locate the AOP
+# files for creating maps later.
+
+
 # DP3.30010.001  high-resolution RGB multispectral imagery
 neonUtilities::byTileAOP(
-  dpID = "DP3.30010.001"
+  dp_id = "DP3.30010.001"
   ,site = "NIWO"
   ,year = "2017"
   ,savepath = "data/data_raw"
